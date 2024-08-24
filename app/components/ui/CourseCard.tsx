981cd90b8f51a6c0472c1f5061d100dd4ba07cd3
Ali <83194640+Ali-Fawzi-Lateef@users.noreply.github.com>
2024-08-24 23:40:30 +0300

    .

--- app/components/ui/CourseCard.tsx
+++ app/components/ui/CourseCard.tsx
@@ -20,12 +20,12 @@ export function CourseCard({course}: {course: course}) {
             <img
                 alt=''
                 src={`${import.meta.env.VITE_API_ENDPOINT}/${course.cover}`}
-                className='object-cover overflow-hidden w-[400px] h-[320px] rounded-xl'
+                className='object-cover overflow-hidden w-[400px] h-[320px] rounded-t-xl'
                 loading='lazy'
             />
             <div className='flex flex-col gap-2 w-full p-4'>
                 <p className='text-brand text-right'>{course.title}</p>
-                <p dir={'RTL'} className='text-xl text-right font-bold min-h-14 truncate'>{course.description}</p>
+                <p dir={'RTL'} className='text-xl text-right font-bold min-h-14 line-clamp-1'>{course.description}</p>
                 <div className='flex flex-row items-center justify-end gap-4'>
                     <div className='flex flex-row items-center justify-end gap-1'>
                         <p className='font-light'>{formatDateToDDMMYYYY(course.startDate)}</p>
--- app/components/ui/WorkshopCard.tsx
+++ app/components/ui/WorkshopCard.tsx
@@ -11,13 +11,13 @@ export function WorkshopCard({workshop}: {workshop: course}) {
             <img
                 alt=''
                 src={`${import.meta.env.VITE_API_ENDPOINT}/${workshop.cover}`}
-                className='object-cover overflow-hidden w-[461px] h-[246px] rounded-xl'
+                className='object-cover overflow-hidden w-[461px] h-[246px] rounded-t-xl'
                 loading='lazy'
             />
             <div className='flex flex-row-reverse items-center justify-between'>
                 <div className='flex flex-col gap-2 w-full px-4 pb-4'>
                     <p className='text-brand text-right'>{workshop.title}</p>
-                    <p dir={'RTL'} className='text-xl text-right font-bold min-h-14'>{workshop.description}</p>
+                    <p dir={'RTL'} className='text-xl text-right font-bold min-h-14 max-w-72 line-clamp-1'>{workshop.description}</p>
                     <div className='flex flex-row items-center justify-end gap-4'>
                         <div className='flex flex-row items-center justify-end gap-1'>
                             <p className='font-light'>{formatDateToDDMMYYYY(workshop.startDate)}</p>
