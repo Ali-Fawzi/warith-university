02ce62a7b52bc886e922a14bb05bfc8d1a7afbfe
Ali <83194640+Ali-Fawzi-Lateef@users.noreply.github.com>
2024-08-26 19:48:59 +0300

    .

--- app/routes/courses.$courseHandle.tsx
+++ app/routes/courses.$courseHandle.tsx
@@ -93,7 +93,7 @@ export default function Courses() {
                                 </button>
                             </Link>
                         )}
-                        {role === 'student' && (
+                        {role === 'Student' && (
                             <button
                                 type='submit'
                                 disabled={isEnrolled}
--- app/routes/workshops.$workshopsHandle.tsx
+++ app/routes/workshops.$workshopsHandle.tsx
@@ -93,7 +93,7 @@ export default function Workshops() {
                                 </button>
                             </Link>
                         )}
-                        {role === 'student' && (
+                        {role === 'Student' && (
                             <button
                                 type='submit'
                                 disabled={isEnrolled}
