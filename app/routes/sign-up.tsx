336783c9256c0a3c94b7a7e4fdb90769ab0d03ea
Ali <83194640+Ali-Fawzi-Lateef@users.noreply.github.com>
2024-09-01 14:24:04 +0300

    .

--- app/routes/sign-up.tsx
+++ app/routes/sign-up.tsx
@@ -5,11 +5,11 @@ import {jwtCookie, roleCookie} from "~/lib/cookies";
 
 export const meta: MetaFunction = () => {
     return [
-        { title: "جامعة وارث الانبياء" },
-        { name: "الوصف", content: "اهلا بكم في جامعة وارث الانبياء في كربلاء المقدسة" },
+        {title: "جامعة وارث الانبياء"},
+        {name: "الوصف", content: "اهلا بكم في جامعة وارث الانبياء في كربلاء المقدسة"},
     ];
 };
-export const action: ActionFunction = async ({ request }) => {
+export const action: ActionFunction = async ({request}) => {
     await new Promise((res) => setTimeout(res, 1000));
     const formData = await request.formData();
     const name = formData.get("name");
@@ -30,7 +30,7 @@ export const action: ActionFunction = async ({ request }) => {
     apiFormData.append("role", userRole as string);
     apiFormData.append("email", email as string);
 
-    if (userRole === "Instructor"){
+    if (userRole === "Instructor") {
         apiFormData.append("jobTitle", jobTitle as string);
         apiFormData.append("linkedIn", linkedIn as string);
         apiFormData.append("website", website as string);
@@ -53,8 +53,8 @@ export const action: ActionFunction = async ({ request }) => {
         return 'Error';
     }
     const response = await res.json();
-    const { token, user } = response;
-    const { role } = user;
+    const {token, user} = response;
+    const {role} = user;
 
     return redirect("/", {
         headers: [
@@ -66,7 +66,7 @@ export const action: ActionFunction = async ({ request }) => {
 export default function SignUp() {
     const actionData = useActionData();
     const navigation = useNavigation();
-    const [selectedOption, setSelectedOption] = useState('Student');
+    const [selectedOption, setSelectedOption] = useState('');
     const state: "idle" | "success" | "error" | "submitting" = navigation.state === "submitting"
         ? "submitting"
         : actionData === 'Error'
@@ -105,57 +105,10 @@ export default function SignUp() {
                     </div>
                     <p className='font-light max-w-xs text-center'>قم بانشاء حساب حتى تتمكن من الاستمرار في استخدام الموقع .</p>
                 </div>
-                <div
-                    className='grid grid-cols-1 xl:grid-cols-2 gap-8 w-full p-4 xl:p-0 text-right max-w-lg xl:max-w-full'>
-                    <div>
-                        <label htmlFor="name"
-                               className="block mb-2 text-sm font-medium">الاسم الكامل</label>
-                        <input type="text"
-                               id="name"
-                               name="name"
-                               className="block p-2.5 w-full text-sm bg-formInput rounded-md border-0"
-                               placeholder="الاسم الكامل"
-                               required
-                               aria-describedby="error-message"
-                               ref={inputRef}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="email"
-                               className="block mb-2 text-sm font-medium">بريدك الالكتروني</label>
-                        <input type="text"
-                               id="email"
-                               name="email"
-                               className="block p-2.5 w-full text-sm bg-formInput rounded-md border-0"
-                               placeholder="البريد الالكتروني" required/>
-                    </div>
-                    <div>
-                        <label htmlFor="username"
-                               className="block mb-2 text-sm font-medium">رقم الهاتف</label>
-                        <input
-                            type="text"
-                            id="username"
-                            className="block p-2.5 w-full text-sm bg-formInput rounded-md border-0"
-                            placeholder="رقم الهاتف"
-                            required
-                            name="username"
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="password"
-                               className="block mb-2 text-sm font-medium">كلمة المرور</label>
-                        <input
-                            type="password"
-                            id="password"
-                            className="block p-2.5 w-full text-sm bg-formInput rounded-md border-0"
-                            placeholder="كلمة المرور"
-                            required
-                            name="password"
-                        />
-                    </div>
+                <div className='grid grid-cols-1 xl:grid-cols-2 gap-8 w-full p-4 xl:p-0 text-right max-w-lg xl:max-w-full'>
                     <div>
                         <label htmlFor="role"
-                               className="block mb-2 text-sm font-medium">اختار دور المستخدم</label>
+                               className="block mb-2 text-sm font-medium">اختر دور المستخدم</label>
                         <select
                             onChange={handleSelectChange}
                             value={selectedOption}
@@ -163,24 +116,90 @@ export default function SignUp() {
                             name='role'
                             className="block p-2.5 w-full text-sm bg-formInput rounded-md border-0"
                         >
+                            <option disabled selected value=''>اختر دور المستخدم</option>
                             <option value="Student">طالب</option>
                             <option value="Instructor">مدرب</option>
                             <option value="Company">شركة</option>
                         </select>
                     </div>
-                    <div>
-                        <label htmlFor="pic"
-                               className="block mb-2 text-sm font-medium">صورة الحساب</label>
-                        <input type="file" id="cover"
-                               name="pic"
-                               className="block w-full bg-formInput rounded-md border-0 shadow-sm text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
+                    {selectedOption === 'Company' && (
+                    <div className='fadeIn'>
+                        <label htmlFor="name"
+                               className="block mb-2 text-sm font-medium">اسم الشركة</label>
+                        <input type="text"
+                               id="name"
+                               name="name"
+                               className="block p-2.5 w-full text-sm bg-formInput rounded-md border-0"
+                               placeholder="اسم الشركة"
+                               required
+                               aria-describedby="error-message"
+                               ref={inputRef}
+                        />
+                    </div>)}
+                    {(selectedOption === "Instructor" || selectedOption === "Student") && (
+                        <div className='fadeIn'>
+                            <label htmlFor="name"
+                                   className="block mb-2 text-sm font-medium">الاسم الكامل</label>
+                            <input type="text"
+                                   id="name"
+                                   name="name"
+                                   className="block p-2.5 w-full text-sm bg-formInput rounded-md border-0"
+                                   placeholder="الاسم الكامل"
+                                   required
+                                   aria-describedby="error-message"
+                                   ref={inputRef}
+                            />
+                        </div>)}
+                    {selectedOption && (
+                        <>
+                            <div className='fadeIn'>
+                                <label htmlFor="email"
+                                       className="block mb-2 text-sm font-medium">بريدك الالكتروني</label>
+                                <input type="text"
+                                       id="email"
+                                       name="email"
+                                       className="block p-2.5 w-full text-sm bg-formInput rounded-md border-0"
+                                       placeholder="البريد الالكتروني" required/>
+                            </div>
+                            <div className='fadeIn'>
+                                <label htmlFor="username"
+                                       className="block mb-2 text-sm font-medium">رقم الهاتف</label>
+                                <input
+                                    type="text"
+                                    id="username"
+                                    className="block p-2.5 w-full text-sm bg-formInput rounded-md border-0"
+                                    placeholder="رقم الهاتف"
+                                    required
+                                    name="username"
+                                />
+                            </div>
+                            <div className='fadeIn'>
+                                <label htmlFor="password"
+                                       className="block mb-2 text-sm font-medium">كلمة المرور</label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    className="block p-2.5 w-full text-sm bg-formInput rounded-md border-0"
+                                    placeholder="كلمة المرور"
+                                    required
+                                    name="password"
+                                />
+                            </div>
+                            <div className='fadeIn'>
+                                <label htmlFor="pic"
+                                       className="block mb-2 text-sm font-medium">صورة الحساب</label>
+                                <input type="file" id="cover"
+                                       name="pic"
+                                       className="block w-full bg-formInput rounded-md border-0 shadow-sm text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
                                 file:bg-formInput file:border-0
                                 file:me-4
                                 file:py-3 file:px-4"
-                               placeholder="صورة الحساب"
-                               required/>
-                    </div>
-                    {selectedOption === "Instructor" && <>
+                                       placeholder="صورة الحساب"
+                                       required/>
+                            </div>
+                        </>
+                    )}
+                    {(selectedOption === "Instructor" || selectedOption === "Company") && <>
                         <div className='fadeIn'>
                             <label htmlFor="jobTitle"
                                    className="block mb-2 text-sm font-medium">العنوان الوظيفي</label>
@@ -233,11 +252,12 @@ export default function SignUp() {
                         </div> : ''}
                     </div>
                     <button
+                        disabled={selectedOption === ''}
                         type='submit'
                         className='inline-block rounded-md font-semibold text-center py-3 px-6 bg-navy text-white hover:bg-navy/90 ease-in-out transform transition duration-500 select-none'>
                         {state === "submitting"
                             ? "يتم انشاء الحساب..."
-                            : "انشاء الحساب"}
+                            : selectedOption ?  "انشاء الحساب" : "اختر دور المستخدم"}
                     </button>
                 </div>
             </Form>
